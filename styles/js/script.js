bc4e3ece9cc53df4cd1daed440c7b6bb5d72d3fd
alreagon <alpharajasa@gmail.com>
2024-03-28 15:58:12 +0700

    Exploration navbar, add scroll listener in script.js

--- styles/js/script.js
+++ styles/js/script.js
@@ -1,24 +1,3 @@
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   observer: true,
-//   observerParents: true,
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 var swiper = new Swiper(".carousel-content", {
   loop: true,
   slidesPerView: "auto",
@@ -30,46 +9,14 @@ var swiper = new Swiper(".carousel-content", {
   },
 });
 
-// const swiper = new Swiper(".carousel-content", {
-//   // Optional parameters
-//   direction: "vertical",
-//   loop: true,
+var nav = document.querySelector("nav");
 
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
-// });
-
-// const swiper = new Swiper(".card-swiper", {
-//   // Optional parameters
-//   direction: "vertical",
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
-// });
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset >= 100) {
+    nav.classList.add("scroll");
+    nav.classList.add("bg-custom-nav", "shadow");
+  } else {
+    nav.classList.remove("scroll");
+    nav.classList.remove("bg-custom-nav", "shadow");
+  }
+});
